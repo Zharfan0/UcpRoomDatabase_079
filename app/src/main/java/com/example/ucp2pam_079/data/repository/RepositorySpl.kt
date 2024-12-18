440edc5f9b127efd5f26082c0206ad86a44bc8b0
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-18 14:51:58 +0700

    membuat kode entitas

--- app/src/main/java/com/example/ucp2pam_079/data/repository/RepositorySpl.kt
+++ app/src/main/java/com/example/ucp2pam_079/data/repository/RepositorySpl.kt
@@ -1,4 +1,15 @@
 package com.example.ucp2pam_079.data.repository
 
-interface RepositorySpl {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "barang")
+data class Barang(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val deskripsi: String,
+    val harga: Double,
+    val stok: Int,
+    val namaSuplier: String
+)
