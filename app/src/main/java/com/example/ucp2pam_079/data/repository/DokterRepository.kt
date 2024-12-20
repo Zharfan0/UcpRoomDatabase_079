34ebc9d08c4b811dc223f294a630fdc5ea0d00f0
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-20 22:49:53 +0700

    membuat class DokterRepository dengan private val pada file DokterRepository.kt

--- app/src/main/java/com/example/ucp2pam_079/data/repository/DokterRepository.kt
+++ app/src/main/java/com/example/ucp2pam_079/data/repository/DokterRepository.kt
@@ -1,2 +1,9 @@
 package com.example.ucp2pam_079.data.repository
 
+import DokterDao
+import com.example.ucp2pam_079.data.entity.Dokter
+
+class DokterRepository (private val dokterDao: DokterDao){
+    suspend fun insertDokter(dokter: Dokter) = dokterDao.insertDokter(dokter)
+    suspend fun getAllDokter() = dokterDao.getAllDokter()
+}
