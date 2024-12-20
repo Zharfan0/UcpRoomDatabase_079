055a65c8404414ae05745aa21023f4b91bdaf364
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-20 22:58:38 +0700

    membuat JadwalviewModel untuk melihat data

--- app/src/main/java/com/example/ucp2pam_079/viewModel/JadwalviewModel.kt
+++ app/src/main/java/com/example/ucp2pam_079/viewModel/JadwalviewModel.kt
@@ -1,2 +1,28 @@
 package com.example.ucp2pam_079.viewModel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam_079.data.entity.Jadwal
+import com.example.ucp2pam_079.data.repository.JadwalRepository
+import kotlinx.coroutines.launch
+
+class JadwalViewModel(private val repository: JadwalRepository) : ViewModel() {
+    fun insertJadwal(jadwal: Jadwal) {
+        viewModelScope.launch {
+            repository.insertJadwal(jadwal)
+        }
+    }
+
+    suspend fun getAllJadwal() = repository.getAllJadwal()
+    fun updateJadwal(jadwal: Jadwal) {
+        viewModelScope.launch {
+            repository.updateJadwal(jadwal)
+        }
+    }
+
+    fun deleteJadwal(jadwal: Jadwal) {
+        viewModelScope.launch {
+            repository.deleteJadwal(jadwal)
+        }
+    }
+}
