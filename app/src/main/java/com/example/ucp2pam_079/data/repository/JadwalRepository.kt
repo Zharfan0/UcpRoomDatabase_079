efad61a90832534d5e3d5def27c1da058d25d24d
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-20 22:51:16 +0700

    membuat suspend fun pada JadwalRepository.kt berupa CRUD

--- app/src/main/java/com/example/ucp2pam_079/data/repository/JadwalRepository.kt
+++ app/src/main/java/com/example/ucp2pam_079/data/repository/JadwalRepository.kt
@@ -1,2 +1,12 @@
 package com.example.ucp2pam_079.data.repository
 
+import JadwalDao
+import com.example.ucp2pam_079.data.entity.Jadwal
+
+class JadwalRepository(private val jadwalDao: JadwalDao) {
+    suspend fun insertJadwal(jadwal: Jadwal) = jadwalDao.insertJadwal(jadwal)
+    suspend fun getAllJadwal() = jadwalDao.getAllJadwal()
+    suspend fun updateJadwal(jadwal: Jadwal) = jadwalDao.updateJadwal(jadwal)
+    suspend fun deleteJadwal(jadwal: Jadwal) = jadwalDao.deleteJadwal(jadwal)
+    suspend fun getJadwalById(id: Int) = jadwalDao.getJadwalById(id)
+}
