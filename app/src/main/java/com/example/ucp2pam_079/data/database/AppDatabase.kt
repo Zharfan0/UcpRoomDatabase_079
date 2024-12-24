e169c8e40dfe911a5c13e550504b856d16faa922
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-24 11:18:22 +0700

    Pembuataan AppDatabase dengan nama DBToko

--- app/src/main/java/com/example/ucp2pam_079/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/ucp2pam_079/data/database/AppDatabase.kt
@@ -4,28 +4,30 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.ucp2pam_079.data.dao.BarangDao
+import com.example.ucp2pam_079.data.dao.SupplierDao
 import com.example.ucp2pam_079.data.entity.Barang
 import com.example.ucp2pam_079.data.entity.Supplier
 
 @Database(entities = [Barang::class, Supplier::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun dokterDao(): DokterDao
-    abstract fun jadwalDao(): JadwalDao
 
-    companion object {
+abstract class TokoDatabase : RoomDatabase(){
+
+    abstract fun barangDao(): BarangDao
+    abstract fun supplierDao(): SupplierDao
+
+    companion object{
         @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "room_database"
-                ).fallbackToDestructiveMigration().build()
-                INSTANCE = instance
-                instance
-            }
+        private var Instance: TokoDatabase? = null
+        fun getDatabase(context: Context): TokoDatabase{
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    TokoDatabase::class.java,
+                    "DBToko"
+                )
+                    .build().also { Instance = it }
+            })
         }
     }
 }
