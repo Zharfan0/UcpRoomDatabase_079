b838373155e67f46192af5ba14cbcd5ba12691ab
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-24 11:17:40 +0700

    Fiksasi SupplierDao.kt (CRUD) dan entitas Supplier (Atribut)

--- app/src/main/java/com/example/ucp2pam_079/data/dao/SupplierDao.kt
+++ app/src/main/java/com/example/ucp2pam_079/data/dao/SupplierDao.kt
@@ -1,4 +1,22 @@
 package com.example.ucp2pam_079.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2pam_079.data.entity.Supplier
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface SupplierDao {
+    @Insert
+    suspend fun insertSupplier(
+        supplier: Supplier
+    )
+
+    @Query("SELECT * FROM supplier ORDER BY namaSup ASC")
+    fun getAllSupplier() : Flow<List<Supplier>>
+
+    @Query("SELECT * FROM supplier WHERE idSup = :idSup")
+    fun getSupplier(idSup: Int): Flow<Supplier>
+
 }
--- app/src/main/java/com/example/ucp2pam_079/data/entity/Supplier.kt
+++ app/src/main/java/com/example/ucp2pam_079/data/entity/Supplier.kt
@@ -1,9 +1,14 @@
 package com.example.ucp2pam_079.data.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "supplier")
 data class Supplier(
-    val namaDokter: String,
-    val namaPasien: String,
-    val tanggalKonsultasi: String,
-    val noHp: String,
-    val status: String
+    @PrimaryKey(autoGenerate = true)
+    val idSup: Int = 0,
+    val namaSup: String,
+    val kontakSup: String,
+    val alamatSup: String
 )
+
