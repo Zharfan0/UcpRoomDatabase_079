7309c87ee3c1566363f43436b8ac33642ce38381
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-24 11:27:12 +0700

    Pembuatan Home View Model

--- app/src/main/java/com/example/ucp2pam_079/viewModel/HomeVM.kt
+++ app/src/main/java/com/example/ucp2pam_079/viewModel/HomeVM.kt
@@ -1,2 +1,13 @@
 package com.example.ucp2pam_079.viewModel
 
+import androidx.lifecycle.ViewModel
+
+class HomeVM(
+): ViewModel(){
+}
+
+data class HomeUiState(
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
